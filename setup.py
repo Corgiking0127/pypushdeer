8d32128b968a5eccc4cabaa2180e2fc2ed33c717
YuFeng <zyf@corgiking.top>
2025-08-04 11:56:48 +0800

    chore(Python): 移除不受支持的版本

--- setup.py
+++ setup.py
@@ -19,9 +19,6 @@ setuptools.setup(
     install_requires=["requests>=2.25.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
